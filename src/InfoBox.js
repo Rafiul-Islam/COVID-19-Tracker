7b6d35d220a452f734859894307b19849dd1a22b
Rafiul Islam <rafi.cse.ru@gmail.com>
2021-04-17 00:02:09 +0600

    Today cases added

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Card, CardContent, Typography} from "@material-ui/core";
 import './App.css'
 
-const InfoBox = ({title, total}) => {
+const InfoBox = ({title, total, cases}) => {
     let cn0 = 'rounded infoBox__cardContent_border'
     let cn1 = 'my-2 infoBox__cardContent_border'
     if (title === 'Cases'){
@@ -21,7 +21,9 @@ const InfoBox = ({title, total}) => {
         <Card className='infoBox'>
             <CardContent className={cn0}>
                 <span className='infoBox__title'>{title}</span>
-                <h3 className={cn1}>+{total / 1000} k</h3>
+                {cases && <h3 className={cn1}>+{cases}</h3>}
+                {cases < 1 && <h3 className={cn1}>0 Cases</h3>}
+                <h3 className="infoBox__total">{(total / 1000).toFixed(1)}k Total</h3>
             </CardContent>
         </Card>
     );
